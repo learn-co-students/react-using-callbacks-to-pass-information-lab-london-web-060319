7d62074a7163a97432bfbf911192270b18f22d26
Federica Macchi <federica.macchi3@gmail.com>
2019-08-06 22:01:54 +0100

    Done

--- src/Cell.js
+++ src/Cell.js
@@ -9,9 +9,15 @@ export default class Cell extends Component {
     }
   }
   
+  changeColor = (color) => {
+    this.setState({
+      color: color
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={() => {this.changeColor(this.props.colorNow())}}>
       </div>
     )
   }
